94200c7b42fac663d3c418bca7f4aa0a8c9422fe
lathamadakkannu <lathamadakkannu@gmail.com>
2023-09-15 14:42:36 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var n=prompt("enter the number");
+ let sum=0; 
+
+ function naturalno(){
+ for(i=1;i<=n;i++){
+    sum=sum+i;
+ }
+ document.write("The sum of given natural number is : "+sum);
+ //return sum;
+}
+naturalno();
+//var result=(naturalno());
+//document.write(result);
